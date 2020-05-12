d06e9bdf4ab7e6963d123f18363ac18cd628c24d
Anna Wojtunik <annawojtunik@gmail.com>
2020-05-12 18:20:23 +0200

    Updates

---
+++ precache-manifest.bd0313eac50cd3254db7d6970abde762.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec7bcd26e0abf6e1084e0637d90c22de",
+    "url": "/video-app/index.html"
+  },
+  {
+    "revision": "2609c4d0f8fe2abfa733",
+    "url": "/video-app/static/css/2.537602a3.chunk.css"
+  },
+  {
+    "revision": "e377a561b6ace2a1702e",
+    "url": "/video-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2609c4d0f8fe2abfa733",
+    "url": "/video-app/static/js/2.417ba99a.chunk.js"
+  },
+  {
+    "revision": "6f35e3df0a05fd289758aba387d3d3d6",
+    "url": "/video-app/static/js/2.417ba99a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e377a561b6ace2a1702e",
+    "url": "/video-app/static/js/main.7e68349f.chunk.js"
+  },
+  {
+    "revision": "1b3a13c8532cf5352166",
+    "url": "/video-app/static/js/runtime-main.b7df5785.js"
+  }
+]);
